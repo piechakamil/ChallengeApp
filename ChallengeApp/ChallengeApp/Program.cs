dc923c27940de957fa6a3307d174d4472138a2f7
piechakamil <piechakamil1@gmail.com>
2023-08-22 23:22:04 +0200

    Dzień 4 upload

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,36 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿// data
+string name = "Ewa";
+char gender = 'w';  // w - woman, m - man
+int age = 33;
+
+// temp var
+string genderTranslated = "";
+
+// decode gender
+if (gender == 'w')
+{
+    genderTranslated = "Kobieta";
+} 
+else if (gender == 'm')
+{
+    genderTranslated = "Mężczyzna";
+}
+
+// core
+if (age > 18 && age < 30)
+{
+    Console.WriteLine($"{genderTranslated} poniżej 30 lat");
+}
+else if (age < 18 && gender == 'm')
+{
+    Console.WriteLine($"Niepełnoletni {genderTranslated}");
+}
+
+else if (name == "Ewa" && age >= 30)
+{
+    Console.WriteLine($"{name}, lat {age}");
+}
+else
+{
+    Console.WriteLine("Takiej osoby nie przewidziano");
+}
